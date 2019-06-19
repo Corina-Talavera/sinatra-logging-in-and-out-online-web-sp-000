d12196fb67367c057f77526bbe10edc4b739f6c4
Corina Talavera <corina.talavera@gmail.com>
2019-06-19 12:23:16 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,15 @@
 class Helpers
+  def self.current_user(session)
+    if session[:user_id] != nil
+      user = User.find(session[:user_id])
+    end
+  end
+
+  def self.is_logged_in?(session)
+    if session[:user_id] == nil
+      false
+    else
+      true
+    end
+  end
 end
